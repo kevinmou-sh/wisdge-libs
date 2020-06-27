942d06ab682633a2d8f2ecd9512c6219caa0dc9e
kevinmou <kevinmou@kuangheng.com>
2020-06-27 16:47:59 +0800

    up to date

--- wisdge.utils/src/com/wisdge/utils/FilenameUtils.java
+++ wisdge.utils/src/com/wisdge/utils/FilenameUtils.java
@@ -27,4 +27,28 @@ public class FilenameUtils extends org.apache.commons.io.FilenameUtils {
                 .replace(":", "");
     }
 
+    /**
+     * 对文件名过滤特殊字符
+     * @param filename
+     * @return
+     */
+    public static String escapePath(String filename) {
+        if (StringUtils.isEmpty(filename))
+            return "";
+
+        return filename.trim().replace(" ", "_")
+                .replace("-", "_")
+                .replace("\\", "/")
+                .replace("+", "")
+                .replace("#", "")
+                .replace("%", "")
+                .replace("&", "")
+                .replace("?", "")
+                .replace("*", "")
+                .replace("@", "")
+                .replace("=", "")
+                .replace("..", "")
+                .replace(":", "");
+    }
+
 }
