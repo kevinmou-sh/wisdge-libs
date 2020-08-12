533636146f7d98cb7de234cba2f3addb3f0b7788
kevinmou <kevinmou@kuangheng.com>
2020-08-12 17:01:53 +0800

    up to date

--- wisdge.utils/src/com/wisdge/utils/PasswordUtils.java
+++ wisdge.utils/src/com/wisdge/utils/PasswordUtils.java
@@ -372,15 +372,19 @@ public class PasswordUtils {
 
 	@Test
 	public void test() throws PasswordInvalidException {
+		String password = "Letmein_0308~";
 		List<String> excludes = new ArrayList<>();
 		excludes.add("kevin");
-		PasswordMatchResult result = PasswordUtils.match("l03080308!~", 8, 20, PasswordUtils.RULE_ALLCASE |
+		PasswordMatchResult result = PasswordUtils.match(password, 8, 20, PasswordUtils.RULE_ALLCASE |
 				PasswordUtils.RULE_DIGIT |
 				PasswordUtils.RULE_SPECIAL |
 				PasswordUtils.RULE_CONTINUOUS_NATURE |
 				PasswordUtils.RULE_CONTINUOUS_KEYBOARD, excludes);
 		System.out.println(result);
 
+		String regexString = "^(?=.*[a-z])(?=.*[A-Z])(?=.*d){8,16}$";
+		Pattern pattern = Pattern.compile(regexString);
+		System.out.println(pattern.matches(regexString, password));
 	}
 }
 
