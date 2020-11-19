586ce698aa45c4bb733915759fa418b7c8b69c38
kevinmou <kevinmou@kuangheng.com>
2020-11-19 17:55:32 +0800

    Up to date

--- wisdge.commons/src/main/java/com/wisdge/commons/redis/IRedisTemplate.java
+++ wisdge.commons/src/main/java/com/wisdge/commons/redis/IRedisTemplate.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 public interface IRedisTemplate {
 	public void set(String key, Object value);
 	public void set(String key, Object value, long ttl, TimeUnit timeUnit);
+	public void putHash(String key, String hashKey, Object hashVaule);
 	public Object get(String key);
 	public Map<Object, Object> entries(String key);
 	public Boolean expire(String key, long timeout, TimeUnit unit);
