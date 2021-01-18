21aa886bb2e9abb164bafb73edda4f7d6820c9c2
lori <loriling01@gmail.com>
2021-01-18 15:34:18 +0800

    up to date

--- wisdge.web/src/com/wisdge/web/filters/XSSPreventionFilter.java
+++ wisdge.web/src/com/wisdge/web/filters/XSSPreventionFilter.java
@@ -30,7 +30,7 @@ public class XSSPreventionFilter implements Filter {
 			"base", "body", "head", "layer", "style", "basefont", "embed", "html", "link", 
 			"title", "bgsound", "frame", "ilayer", "meta", "script"};
 	private static final String[] filterAttributes = {"dynsrc", "action", "background", "bgsound", "lowsrc",
-			"value", "onmouse(.*?)"}; // src和href暂时不过滤
+			"value"}; // src和href暂时不过滤
 	private static final String[] filterKeywords = {"vbscript:", "ms-its:", "firefoxurl:", "javascript:", 
 			"mhtml:", "mocha:", "data:", "livescript:"};
 	
@@ -168,7 +168,7 @@ public class XSSPreventionFilter implements Filter {
 	public void init(FilterConfig filterConfig) throws ServletException {
 		this.filterConfig = filterConfig;
 	}
-	
+
 	public static String filter(String value) {
 		String cleanValue = Normalizer.normalize(value, Normalizer.Form.NFD);
 
@@ -213,7 +213,111 @@ public class XSSPreventionFilter implements Filter {
 			scriptPattern = Pattern.compile("</" + filterLabel + ">", Pattern.CASE_INSENSITIVE);
 			cleanValue = scriptPattern.matcher(cleanValue).replaceAll(" ");
 		}
-		
+		cleanValue = cleanEventAttact(cleanValue);
+		cleanValue = cleanValue.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
 		return cleanValue;
+	}
+
+	public static String cleanEventAttact(String value) {
+		//避免οnclick= 表达式
+		Pattern compile = Pattern.compile("onafterprint(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onbeforeprint(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onbeforeunload(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onerror(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onhaschange(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onload(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onmessage(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onoffline(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("ononline(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onpagehide(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onpageshow(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onpopstate(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onredo(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onresize(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onstorage(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onundo(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onunload(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onblur(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onchange(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("oncontextmenu(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onfocus(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onformchange(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onforminput(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("oninput(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("oninvalid(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onreset(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onselect(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onsubmit(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onkeydown(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onkeypress(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onkeyup(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onclick(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("ondblclick(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("ondrag(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("ondragend(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("ondragenter(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("ondragleave(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("ondragover(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("ondragstart(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("ondrop(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onmousedown(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onmousemove(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onmouseout(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onmouseover(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onmouseenter(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onmouseup(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onmousewheel(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		compile = Pattern.compile("onscroll(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+		value = compile.matcher(value).replaceAll("");
+		value = value.replace("document", "");//页面屏蔽document字样
+		value = value.replace("alert", "");//页面屏蔽alert字样
+		return value;
 	}
 }
