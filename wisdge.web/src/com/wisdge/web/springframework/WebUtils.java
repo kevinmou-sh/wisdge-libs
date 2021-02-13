f8bd72d290cfe0b481c31ab867398965cf7923a3
kevinmou <kevinmou@kuangheng.com>
2021-02-13 13:00:12 +0800

    Up to date

--- wisdge.web/src/com/wisdge/web/springframework/WebUtils.java
+++ wisdge.web/src/com/wisdge/web/springframework/WebUtils.java
@@ -27,6 +27,16 @@ import com.wisdge.utils.ByteUtils;
 public class WebUtils extends org.springframework.web.util.WebUtils {
 	private static final Log logger = LogFactory.getLog(WebUtils.class);
 
+	/**
+	 * 从request请求中读取payload字符串。不同于getRequestPayload, 该方法支持重复读取
+	 * @param request HttpServletRequest
+	 * @return String
+	 * @throws IOException
+	 */
+	public static String getPayload(HttpServletRequest request) throws IOException {
+		return new MultiReadHttpServletRequest(request).getPayload();
+	}
+
 	public static String getRequestPayload(HttpServletRequest request) {
 		StringBuilder builder = new StringBuilder();
 		try {
@@ -55,77 +65,77 @@ public class WebUtils extends org.springframework.web.util.WebUtils {
 			return Collections.emptyMap();
 		}
 	}
-	
+
 	public static <T> T getRequestPayloadBean(HttpServletRequest request, Class<T> beanClass) throws JsonParseException, JsonMappingException, IOException {
 		String payload = getRequestPayload(request);
 		return JSonUtils.read(payload, beanClass);
 	}
-	
+
 	public static String getContentType(String extension) throws IOException {
 		InputStream is = WebUtils.class.getClassLoader().getResourceAsStream("contentType.properties");
 		Properties properties = new Properties();
 		properties.load(is);
 		return properties.getProperty(extension);
 	}
-	
+
 	public static String getString(HttpServletRequest request, String parameter) {
 		return getString(request, parameter, "");
 	}
-	
+
 	public static String getString(HttpServletRequest request, String parameter, String defaultValue) {
 		String value = request.getParameter(parameter);
 		if (StringUtils.isEmpty(value))
 			return defaultValue;
 		return value;
 	}
-	
+
 	public static Boolean getBoolean(HttpServletRequest request, String parameter) {
 		String value = request.getParameter(parameter);
 		if (StringUtils.isEmpty(value))
 			return false;
 		return Boolean.parseBoolean(value);
 	}
-	
+
 	public static long getLong(HttpServletRequest request, String parameter) {
 		return getLong(request, parameter, 0L);
 	}
-	
+
 	public static long getLong(HttpServletRequest request, String parameter, long defaultValue) {
 		String value = request.getParameter(parameter);
 		if (StringUtils.isEmpty(value))
 			return defaultValue;
 		return Long.parseLong(value);
 	}
-	
+
 	public static int getInteger(HttpServletRequest request, String parameter) {
 		return getInteger(request, parameter, 0);
 	}
-	
+
 	public static int getInteger(HttpServletRequest request, String parameter, int defaultValue) {
 		String value = request.getParameter(parameter);
 		if (StringUtils.isEmpty(value))
 			return defaultValue;
 		return Integer.parseInt(value);
 	}
-	
+
 	public static double getDouble(HttpServletRequest request, String parameter) {
 		return getDouble(request, parameter, 0D);
 	}
-	
+
 	public static double getDouble(HttpServletRequest request, String parameter, double defaultValue) {
 		String value = request.getParameter(parameter);
 		if (StringUtils.isEmpty(value))
 			return defaultValue;
 		return Double.parseDouble(value);
 	}
-	
+
 	public static void removeSessionAttribute(HttpServletRequest request, String name) {
 		Assert.notNull(request, "Request must not be null");
 		HttpSession session = request.getSession(false);
 		if (session != null)
 			session.removeAttribute(name);
 	}
-	
+
 	public static byte[] getFile(HttpServletRequest request, String filename) {
 		if (request instanceof MultipartHttpServletRequest) {
 			MultipartHttpServletRequest mr = (MultipartHttpServletRequest) request;
@@ -142,51 +152,51 @@ public class WebUtils extends org.springframework.web.util.WebUtils {
 		return null;
 	}
 
-    /** 
-     * 断点续传支持 
-     * @param file 
-     * @param request 
-     * @param response 
-     * @return 跳过多少字节 
-     */ 
-    private static RangeSettings getRange(byte[] data, HttpServletRequest request) {  
+    /**
+     * 断点续传支持
+     * @param file
+     * @param request
+     * @param response
+     * @return 跳过多少字节
+     */
+    private static RangeSettings getRange(byte[] data, HttpServletRequest request) {
         String range = request.getHeader("Range");
         if (range == null)
-        	return new RangeSettings(0, data.length, data.length, data.length);  
-        return getSettings(data.length, range.replaceAll("bytes=", ""));  
+        	return new RangeSettings(0, data.length, data.length, data.length);
+        return getSettings(data.length, range.replaceAll("bytes=", ""));
     }
-   
-    private static RangeSettings getSettings(long len, String range) {  
-        long contentLength = 0;  
-        long start = 0;  
-        long end = 0;  
-        if (range.startsWith("-"))// -500，最后500个  
-        {  
-             contentLength = Long.parseLong(range.substring(1));//要下载的量  
-             end = len-1;  
-             start = len - contentLength;  
-        }  
-        else if (range.endsWith("-"))//从哪个开始  
-        {  
-            start = Long.parseLong(range.replace("-", ""));  
-            end = len -1;  
-            contentLength = len - start;  
-        }  
-        else//从a到b  
-        {  
-            String[] se = range.split("-");  
-            start = Long.parseLong(se[0]);  
-            end = Long.parseLong(se[1]);  
-            contentLength = end-start+1;  
-        }  
-        return new RangeSettings(start, end, contentLength, len);  
+
+    private static RangeSettings getSettings(long len, String range) {
+        long contentLength = 0;
+        long start = 0;
+        long end = 0;
+        if (range.startsWith("-"))// -500，最后500个
+        {
+             contentLength = Long.parseLong(range.substring(1));//要下载的量
+             end = len-1;
+             start = len - contentLength;
+        }
+        else if (range.endsWith("-"))//从哪个开始
+        {
+            start = Long.parseLong(range.replace("-", ""));
+            end = len -1;
+            contentLength = len - start;
+        }
+        else//从a到b
+        {
+            String[] se = range.split("-");
+            start = Long.parseLong(se[0]);
+            end = Long.parseLong(se[1]);
+            contentLength = end-start+1;
+        }
+        return new RangeSettings(start, end, contentLength, len);
     }
-    
+
 	public static void out(HttpServletRequest request, HttpServletResponse response, byte[] data, String filename, Boolean download) {
 		if (data == null)
 			data = new byte[0];
-		
-        RangeSettings rangeSettings = getRange(data, request);  
+
+        RangeSettings rangeSettings = getRange(data, request);
 		try {
 			//response.reset();
 			//response.setHeader("Pragma", "no-cache");
@@ -203,7 +213,7 @@ public class WebUtils extends org.springframework.web.util.WebUtils {
 				response.addHeader("Content-Length", "" + data.length);
 				response.setContentType(WebUtils.getContentType(FilenameUtils.getExtension(filename)));
 			}
-			
+
 			if (!rangeSettings.isRange()) {
 				response.addHeader("Content-Length", String.valueOf(rangeSettings.getTotalLength()));
 			} else {
@@ -227,7 +237,7 @@ public class WebUtils extends org.springframework.web.util.WebUtils {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void setContentDisposition(HttpServletRequest request, HttpServletResponse response, String filename, boolean inline) throws UnsupportedEncodingException {
 		filename = URLEncoder.encode(filename, "UTF-8").replace("+", "%20");
 		String type = inline ? "inline" : "attachment";
@@ -235,67 +245,67 @@ public class WebUtils extends org.springframework.web.util.WebUtils {
 				+ "filename=\"" + filename + "\";"
 				+ "filename*=UTF-8''" + filename);
 	}
-	
+
 	public static void setContentDisposition(HttpServletRequest request, HttpServletResponse response, String filename) throws UnsupportedEncodingException {
 		setContentDisposition(request, response, filename, false);
 	}
 
 }
 
-class RangeSettings{  
-    private long start;  
-    private long end;  
-    private long contentLength;  
-    private long totalLength;  
-  
-    public RangeSettings(){  
-        super();  
-    }  
+class RangeSettings{
+    private long start;
+    private long end;
+    private long contentLength;
+    private long totalLength;
 
-    public RangeSettings(long start, long end, long contentLength,long totalLength) {  
-        this.start = start;  
-        this.end = end;  
-        this.contentLength = contentLength;  
-        this.totalLength = totalLength;  
-    }  
+    public RangeSettings(){
+        super();
+    }
 
-    public RangeSettings(long totalLength) {  
-        this.totalLength = totalLength;  
-    }  
+    public RangeSettings(long start, long end, long contentLength,long totalLength) {
+        this.start = start;
+        this.end = end;
+        this.contentLength = contentLength;
+        this.totalLength = totalLength;
+    }
 
-    public long getStart() {  
-        return start;  
-    }  
+    public RangeSettings(long totalLength) {
+        this.totalLength = totalLength;
+    }
 
-    public void setStart(long start) {  
-        this.start = start;  
-    }  
+    public long getStart() {
+        return start;
+    }
 
-    public long getEnd() {  
-        return end;  
-    }  
+    public void setStart(long start) {
+        this.start = start;
+    }
 
-    public void setEnd(long end) {  
-        this.end = end;  
-    }  
+    public long getEnd() {
+        return end;
+    }
 
-    public long getContentLength() {  
-        return contentLength;  
-    }  
+    public void setEnd(long end) {
+        this.end = end;
+    }
 
-    public void setContentLength(long contentLength) {  
-        this.contentLength = contentLength;  
-    }  
+    public long getContentLength() {
+        return contentLength;
+    }
 
-    public long getTotalLength() {  
-        return totalLength;  
-    }  
+    public void setContentLength(long contentLength) {
+        this.contentLength = contentLength;
+    }
 
-    public void setTotalLength(long totalLength) {  
-        this.totalLength = totalLength;  
-    }  
+    public long getTotalLength() {
+        return totalLength;
+    }
 
-    public boolean isRange() {  
-        return (start > 0 || contentLength < totalLength);  
-    }  
+    public void setTotalLength(long totalLength) {
+        this.totalLength = totalLength;
+    }
+
+    public boolean isRange() {
+        return (start > 0 || contentLength < totalLength);
+    }
 }
