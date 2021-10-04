d4f9ee04aa4bf88edaaa65d65f02bf2065c38950
Kevin MOU <kevin.mou@outlook.com>
2021-10-04 16:03:10 +0800

    Up to date

--- wisdge.dataservice/src/main/java/com/wisdge/dataservice/sql/page/PageJdbcTemplate.java
+++ wisdge.dataservice/src/main/java/com/wisdge/dataservice/sql/page/PageJdbcTemplate.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import java.sql.ResultSetMetaData;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 public class PageJdbcTemplate {
@@ -16,7 +18,43 @@ public class PageJdbcTemplate {
 
     public Pagination queryForPage(PageInfo pageInfo, String sql, Object[] params) {
         int totalRows = jdbcTemplate.queryForObject(PageHelper.getCountString(sql), params, Integer.class);
+        int pageSize = pageInfo.getPageSize();
+        int pageIndex = pageInfo.getPageIndex();
+        if (pageIndex < 1)
+            pageIndex = 1;
+        if (pageSize >= 5000) {
+            log.warn("Page query size has overhead 5000, actually is {}", + pageSize);
+        }
+        Pagination pagination = new Pagination(totalRows, pageIndex, pageSize);
+        int offset = (pagination.getPageIndex() - 1) * pagination.getPageSize();
+        String pageSql = PageHelper.getLimitString(sql, offset, pageSize);
+        // log.debug("Query page: {}", pageSql);
+        return jdbcTemplate.query(pageSql, params, rse -> {
+            // 获取当前记录集的字段数据
+            List<String> columns = new ArrayList<>();
+            ResultSetMetaData rsmd = rse.getMetaData();
+            for (int column = 1; column <= rsmd.getColumnCount(); column++) {
+                columns.add(rsmd.getColumnLabel(column).toUpperCase());
+            }
+            pagination.setColumns(columns);
 
+            // 获取当前记录集的字段数据
+            List<Map<String, Object>> fields = new ArrayList<>();
+            while(rse.next()) {
+                Map<String, Object> row = new HashMap<>();
+                for(String column : columns) {
+                    row.put(column, rse.getObject(column));
+                }
+                fields.add(row);
+            }
+            pagination.setFields(fields);
+
+            return pagination;
+        });
+    }
+
+    public Pagination queryForPageLegacy(PageInfo pageInfo, String sql, Object[] params) {
+        int totalRows = jdbcTemplate.queryForObject(PageHelper.getCountString(sql), params, Integer.class);
         int pageSize = pageInfo.getPageSize();
         int pageIndex = pageInfo.getPageIndex();
         if (pageIndex < 1)
