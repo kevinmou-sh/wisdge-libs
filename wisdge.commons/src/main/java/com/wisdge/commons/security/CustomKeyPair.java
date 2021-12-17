b98c31a3f3683b123d64fa984916c89d901d2e72
kevinmou <kevinmou@kuangheng.com>
2021-12-17 12:24:46 +0800

    Up to date

--- wisdge.commons/src/main/java/com/wisdge/commons/security/CustomKeyPair.java
+++ wisdge.commons/src/main/java/com/wisdge/commons/security/CustomKeyPair.java
@@ -3,14 +3,13 @@ package com.wisdge.commons.security;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.rsa.crypto.KeyStoreKeyFactory;
 import org.springframework.stereotype.Component;
-
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.Base64;
 
 @Data
 @Component
@@ -37,4 +36,21 @@ public class CustomKeyPair {
     public PrivateKey getPrivate() {
         return getKeyPair().getPrivate();
     }
+
+    public String getPrivateString() {
+        return Base64.getEncoder().encodeToString(getPrivate().getEncoded());
+    }
+
+    public String getPublicString() {
+        return Base64.getEncoder().encodeToString(getPublic().getEncoded());
+    }
+
+    public static String getPrivate(KeyPair keyPair) {
+        return Base64.getEncoder().encodeToString(keyPair.getPrivate().getEncoded());
+    }
+
+    public static String getPublic(KeyPair keyPair) {
+        return Base64.getEncoder().encodeToString(keyPair.getPublic().getEncoded());
+    }
+
 }
