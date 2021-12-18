63529cd9f757e75f7462cbbba55e23ee323e001a
kevinmou <kevinmou@kuangheng.com>
2021-12-18 13:47:52 +0800

    重新整理pom引用

--- wisdge.utils/src/main/java/com/wisdge/utils/FileUtils.java
+++ wisdge.utils/src/main/java/com/wisdge/utils/FileUtils.java
@@ -1,9 +1,3 @@
-/**
- * Description: File utility classes
- * Copyright: (c)2014 Wisdge.com
- * @author Kevin MOU
- * @version 1.14
- */
 package com.wisdge.utils;
 
 import java.io.File;
@@ -22,7 +16,7 @@ public class FileUtils extends org.apache.commons.io.FileUtils {
 	/**
 	 * 获得一个自增长序号的文件。<br/>
 	 * 该方法通常用于对于已经存在的文件不采用覆盖的方式，而是自动增加序号在文件命中。
-	 * 
+	 *
 	 * @param filepath
 	 *            文件路径
 	 * @return File
@@ -45,13 +39,13 @@ public class FileUtils extends org.apache.commons.io.FileUtils {
 		} while (file.exists());
 		return file;
 	}
-	
+
 	public static String read(InputStream is) throws IOException {
 		byte[] buffer = new byte[is.available()];
 		IOUtils.read(is, buffer);
 		return new String(buffer);
 	}
-	
+
 	public static FileInputStream openInputStream(String filepath) throws Exception {
 		File file = new File(filepath);
 		if (file.exists() && !file.isDirectory() && file.canRead()) {
