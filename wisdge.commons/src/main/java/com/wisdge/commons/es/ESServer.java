9f00dafba519dbff37d253a347a635141f7c290c
Kevin MOU <kevin.mou@outlook.com>
2021-12-21 18:30:35 +0800

    Up to date

--- wisdge.commons/src/main/java/com/wisdge/commons/es/ESServer.java
+++ wisdge.commons/src/main/java/com/wisdge/commons/es/ESServer.java
@@ -1,25 +1,10 @@
 package com.wisdge.commons.es;
 
+import lombok.Data;
+
+@Data
 public class ESServer {
 	private String host = "127.0.0.1";
     private int port = 9200;
     private String schema = "http";
-	public String getHost() {
-		return host;
-	}
-	public void setHost(String host) {
-		this.host = host;
-	}
-	public int getPort() {
-		return port;
-	}
-	public void setPort(int port) {
-		this.port = port;
-	}
-	public String getSchema() {
-		return schema;
-	}
-	public void setSchema(String schema) {
-		this.schema = schema;
-	}
 }
