2de70b13134ec9d24c05b1d9a18928d130e61d23
kevinmou <kevinmou@kuangheng.com>
2021-12-21 14:11:13 +0800

    Up to date

--- wisdge.utils/src/main/java/com/wisdge/utils/LogUtils.java
+++ wisdge.utils/src/main/java/com/wisdge/utils/LogUtils.java
@@ -6,10 +6,9 @@ public class LogUtils {
 
     public static Object forging(Object object) {
         String encode = Normalizer.normalize(object.toString(), Normalizer.Form.NFKC);
-        encode.replace("%0d", "")
+        return encode.replace("%0d", "")
                 .replace("\r", "")
                 .replace("%0a", "")
                 .replace("\n", "");
-        return encode;
     }
 }
