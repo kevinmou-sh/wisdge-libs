b6f44c5a1800d284200f68cb103febe2103e9e1a
kevinmou <kevinmou@kuangheng.com>
2022-01-18 19:36:27 +0800

    Up to date

--- wisdge.web/src/main/java/com/wisdge/eventcluster/Event.java
+++ wisdge.web/src/main/java/com/wisdge/eventcluster/Event.java
@@ -1,30 +1,23 @@
 package com.wisdge.eventcluster;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.Date;
 
+@Data
 public class Event implements Serializable {
-	private static final long serialVersionUID = 1L;
-	private final String id;
-	private final Object value;
-	private final long timestamp;
-	
+	private String id;
+	private Object value;
+	private long timestamp;
+
+	public Event() {
+		this.timestamp = new Date().getTime();
+	}
+
 	public Event(String eventId, Object value) {
 		this.id = eventId;
 		this.value = value;
 		this.timestamp = new Date().getTime();
 	}
-
-	public String getId() {
-		return id;
-	}
-
-	public Object getValue() {
-		return value;
-	}
-
-	public long getTimestamp() {
-		return timestamp;
-	}
-
 }
