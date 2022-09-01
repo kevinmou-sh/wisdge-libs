1cd99fd1a428dc637b6536f05b42f55eb2719ccd
Kevin MOU <kevin.mou@outlook.com>
2022-09-02 00:03:16 +0800

    Up to date

--- wisdge.ezcell/src/main/java/com/wisdge/ezcell/analysis/v07/XlsxSaxAnalyser.java
+++ wisdge.ezcell/src/main/java/com/wisdge/ezcell/analysis/v07/XlsxSaxAnalyser.java
@@ -43,7 +43,7 @@ public class XlsxSaxAnalyser extends BaseSaxAnalyser {
     private void init() throws IOException, OpenXML4JException, XmlException {
         analysisContext.setCurrentRowNo(0);
         xssfReader = new XSSFReader(OPCPackage.open(analysisContext.getInputStream()));
-        sharedStringsTable = (SharedStringsTable) xssfReader.getSharedStringsTable();
+        sharedStringsTable = xssfReader.getSharedStringsTable();
         stylesTable = xssfReader.getStylesTable();
 
         InputStream workbookXml = xssfReader.getWorkbookData();
